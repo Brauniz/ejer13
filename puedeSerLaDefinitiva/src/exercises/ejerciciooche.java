6a6ffeeca23181a8d7b7434e86ed1101ccaa9f8d
omar <omarcajmer@gmail.com>
2024-02-22 02:12:56 +0100

    v .03

---
+++ puedeSerLaDefinitiva/src/exercises/ejerciciooche.java
@@ -0,0 +1,25 @@
+package exercises;
+public class ejerciciooche {
+
+	public static void main(String[] args) {
+		cocheObjeto c1 = new cocheObjeto("Brauniz","nissan","gtr",19000,"02/07/2002",199990.0 );
+		c1.datos();
+		c1.precio_base();
+		c1.año_matriculacion();
+		c1.bisisesto();
+		c1.precio();
+		c1.primo();
+		c1.road_to_200000();
+		c1.contadorCaracteres();
+		c1.masUsado();
+		c1.caro();
+		
+		cocheObjeto c2 = new cocheObjeto("Brauniz", 45000, "05/07/1890", 178);
+		
+		
+		
+		
+		
+	}
+
+}
